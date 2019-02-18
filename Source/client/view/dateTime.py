140f74f7f9df36bdcfb06da7d69e39fe5d919f0f
Estelle <owasana@hotmail.fr>
2019-02-18 16:23:41 +0100

    ajout de fonctions dans datetime.py

--- Source/client/view/dateTime.py
+++ Source/client/view/dateTime.py
@@ -2,17 +2,43 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class DateTimeBox(Gtk.Dialog):
+class DateTimeDialog(Gtk.Dialog):
 
     def __init__(self, parent):
         Gtk.Dialog.__init__(self, "Date & Time", parent, 0, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK))
+        self.time_box = TimeBox()
 
         self.set_default_size(200, 200)
-        calendar = Gtk.Calendar()
+        self.calendar = Gtk.Calendar()
         main_box = self.get_content_area()
-        main_box.add(calendar)
-        main_box.add(TimeBox())
+        main_box.add(self.calendar)
+        main_box.add(self.time_box)
         self.show_all()
+
+
+        @property
+        def hour(self):
+            return self.time_box.spin_hour.get_value_as_int()
+
+        @property
+        def hour(self):
+            return self.time_box.spin_minute.get_value_as_int()
+
+        @property
+        def year(self):
+            year , month , day = self.calendar.get_date()
+            return year
+
+        @property
+        def month(self):
+            year , month , day = self.calendar.get_date()
+            return month
+
+        @property
+        def day(self):
+            year , month , day = self.calendar.get_date()
+            return day
+
 
 
 class TimeBox(Gtk.Box):
@@ -24,13 +50,13 @@ class TimeBox(Gtk.Box):
         label = Gtk.Label(" h ")
 
         adjustment_hour = Gtk.Adjustment(0,0, 23, 1, 10, 0)
-        hour = Gtk.SpinButton()
-        hour.set_adjustment(adjustment_hour)
+        self.spin_hour = Gtk.SpinButton()
+        self.spin_hour.set_adjustment(adjustment_hour)
 
         adjustment_minute = Gtk.Adjustment(0,0, 59, 1 , 10, 0)
-        minute = Gtk.SpinButton()
-        minute.set_adjustment(adjustment_minute)
+        self.spin_minute = Gtk.SpinButton()
+        self.spin_minute.set_adjustment(adjustment_minute)
 
-        self.add(hour)
+        self.add(self.spin_hour)
         self.add(label)
-        self.add(minute)
+        self.add(self.spin_minute)
