c674a1a24cde42b2b675c1f02dff70877a23779e
tristan <tristan@upbge.org>
2019-02-19 10:35:39 +0100

    Ajout de fichiers non suivis.

---
+++ Source/client/view/centerBox.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+from client.model import common
+from .agenda import *
+from .addevent import *
+
+class CenterBox(Gtk.Box):
+    """Boîte contenant tout ce qui est affiché à l'écran"""
+    def __init__(self, common):
+        self.common = common
+
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL, spacing=6)
+
+        self.add(AgendaBox(self.common))
+        self.add(AddEventButton(None, self.common))
+
+        self.show_all()
