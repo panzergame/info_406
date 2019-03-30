9f4af534460f75bdf05e2b2874734db43b7e0493
tristan <tristan@upbge.org>
2019-03-30 16:03:03 +0100

    Gestion de la configuration dans ClientCollection.

--- Source/client/model/collection.py
+++ Source/client/model/collection.py
@@ -4,9 +4,19 @@ from db import *
 
 import mysql.connector
 
+# TEMP
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("config.xml")
+root = tree.getroot()
+
+login = root.find("login").text
+password = root.find("password").text
+# TEMP
+
 class ClientCollection(DbCollection):
 	def __init__(self):
-		self.conn = mysql.connector.connect(host="localhost", user="root", password="root", database="info_406")
+		self.conn = mysql.connector.connect(host="localhost", user=login, password=password, database="info_406")
 		self.cursor = self.conn.cursor()
 
 		super().__init__(self.cursor)
