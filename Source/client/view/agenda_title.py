1435ae0f38a502f275f5187313c917e370187f21
NILS <nilsruet@gmx.fr>
2019-03-31 20:57:52 +0200

    Titre d'un agenda est désormais centré

--- Source/client/view/agenda_title.py
+++ Source/client/view/agenda_title.py
@@ -5,15 +5,18 @@ from gi.repository import Gtk
 class AgendaTitleBox(Gtk.Box):
 	def __init__(self, common):
 		Gtk.Box.__init__(self)
+		self.set_halign(Gtk.Align.CENTER)
+		frame=Gtk.Frame()
 		self.common = common
 		self.title = Gtk.Label()
 		self.setTitleString()
-		self.add(self.title)
+		frame.add(self.title)
+		self.add(frame)
 
 		common.add_observer(self)
 	
 	def setTitleString(self):
-		if self.common.agenda_displayed.user!=self.common.user_clicked:
+		if self.common.agenda_displayed.user == None:
 			self.title.set_text("{} ({})".format(self.common.agenda_displayed.name, self.common.agenda_displayed.group.name))
 		else:
 			self.title.set_text("Agenda de {} {}".format(self.common.agenda_displayed.user.first_name,self.common.agenda_displayed.user.last_name))
