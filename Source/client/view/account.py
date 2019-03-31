b1e3e92f2fee9d85a449a130e56c11478c9f675b
tristan <tristan@upbge.org>
2019-03-31 20:42:45 +0200

    Utilisation d'une liste pour les utilisateurs.

--- Source/client/view/account.py
+++ Source/client/view/account.py
@@ -7,37 +7,41 @@ from core import *
 from client.model.common import *
 
 
-class AccountBox(Gtk.Box):
+class AccountBox(Gtk.VBox):
 
 	def __init__(self, common):
+		super().__init__()
 
 		self.common = common
 
-		Gtk.Box.__init__(self,orientation=Gtk.Orientation.VERTICAL, spacing=6)
-		self.set_border_width(10)
-
-		title = Gtk.Label("Sélectionner un utilisateur")
+		title = Gtk.Label("Utilisateurs", xalign=0)
 		self.pack_start(title, True, True, 0)
 
-		users = common.account.users
-		first = True
-		group = None
-		for user in users:
-			name = user.first_name + " " + user.last_name
-			if first:
-				button = Gtk.RadioButton.new_with_label_from_widget(None, name)
-				first = False
-				group = button
-				self.common.user_clicked = user
-				self.common.agenda_displayed = user.agenda
-			else:
-				button = Gtk.RadioButton.new_with_label_from_widget(group, name)
-			button.connect("toggled", self.on_button_toggled, user)
-			self.pack_start(button, False, False, 0)
+		self.list = Gtk.ListStore(str, str, object)
 
+		view = Gtk.TreeView(self.list)
 
-	def on_button_toggled(self, button, user):
-		if button.get_active():
+		first_name = Gtk.CellRendererText()
+		last_name = Gtk.CellRendererText()
+		name_column = Gtk.TreeViewColumn("Nom")
+		name_column.pack_start(first_name, True)
+		name_column.pack_start(last_name, True)
+		name_column.add_attribute(first_name, "text", 0)
+		name_column.add_attribute(last_name, "text", 1)
+
+		view.append_column(name_column)
+
+		select = view.get_selection()
+		select.connect("changed", self.on_user_changed)
+
+		self.add(view)
+
+		for user in common.account.users:
+			self.list.append((user.first_name, user.last_name, user))
+
+	def on_user_changed(self, selection):
+		model, iter = selection.get_selected()
+		if iter is not None:
+			user = model[iter][2]
 			self.common.user_clicked = user
 			self.common.agenda_displayed = user.agenda
-
