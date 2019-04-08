0d5f9d2ed29311560349ccc0ed28b3fc29eddd08
Zdepu <36131812+Zdepu@users.noreply.github.com>
2019-04-08 10:39:40 +0200

    Structure graphique de la gestion d'utilisateur
    3 boutons et la possibilté de chocher un ou des users pour la sélection

--- Source/client/view/account.py
+++ Source/client/view/account.py
@@ -16,17 +16,22 @@ class AccountBox(Gtk.VBox):
 		self.common.add_observer(self)
 
 		title = Gtk.Label("Utilisateurs", xalign=0)
+		add_user_button = Gtk.Button(label="Ajouter")
+		add_user_button.connect("clicked", self.on_add_user_clicked)
 		self.pack_start(title, True, True, 0)
+		self.pack_start(add_user_button, True, True, 0)
 
 		self.list = Gtk.ListStore(str, str, object)
 
 		view = Gtk.TreeView(self.list)
 
+		selected = Gtk.CellRendererToggle()
 		first_name = Gtk.CellRendererText()
 		last_name = Gtk.CellRendererText()
 		name_column = Gtk.TreeViewColumn("Nom")
 		name_column.pack_start(first_name, True)
 		name_column.pack_start(last_name, True)
+		name_column.pack_start(selected, True)
 		name_column.add_attribute(first_name, "text", 0)
 		name_column.add_attribute(last_name, "text", 1)
 
@@ -36,6 +41,12 @@ class AccountBox(Gtk.VBox):
 		self.group_list = GroupList(self.common)
 
 		self.add(view)
+		del_user_button = Gtk.Button(label="Supprimer")
+		del_user_button.connect("clicked", self.on_del_user_clicked)
+		up_user_button = Gtk.Button(label="Modifier")
+		up_user_button.connect("clicked", self.on_up_user_clicked)
+		self.add(del_user_button)
+		self.add(up_user_button)
 		self.add(Gtk.Label("Mes groupes", xalign=0))
 		self.add(self.group_list)
 
@@ -48,5 +59,12 @@ class AccountBox(Gtk.VBox):
 		self.common.agenda_displayed = user.agenda
 		self.group_list.set_groups(user.groups)
 
+	def on_add_user_clicked(self, widget):
+		print("add")
+
+	def on_del_user_clicked(self, widget):
+		print("supp")
+	def on_up_user_clicked(self, widget):
+		print("modif")
 	def update(self, common):
 		self.group_list.set_groups(self.common.user_clicked.groups)
