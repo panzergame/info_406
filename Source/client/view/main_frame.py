58edb4af7289b49793f2863a0b5fb67e95cd05c5
owasana <owasana@debian>
2019-05-15 10:51:53 +0200

    début de CSS

--- Source/client/view/main_frame.py
+++ Source/client/view/main_frame.py
@@ -2,11 +2,27 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk , Gdk
 
 from .main_box import *
 
 class MainFrame(Gtk.Window):
     def __init__(self, common):
+
         Gtk.Window.__init__(self, title="Votre Agenda")
         self.add(MainBox(common))
+        self.set_name("MainFrame")
+        #pour la démo:
+        #self.fullscreen()
+
+
+
+        #Initialisation d'un fichier CSS:
+        style_provider = Gtk.CssProvider()
+        css = open('client/view/style.css' ,'rb')
+        css_data = css.read()
+        css.close()
+
+        style_provider.load_from_data(css_data)
+
+        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),style_provider,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
