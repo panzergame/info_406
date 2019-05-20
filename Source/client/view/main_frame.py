0cae608c4aae9a0c23ee7683492808ba8d5c6bd7
NILS <nilsruet@gmx.fr>
2019-05-20 09:44:23 +0200

    La fenêtre principale s'affiche désormais en taille maximale

--- Source/client/view/main_frame.py
+++ Source/client/view/main_frame.py
@@ -7,21 +7,22 @@ from gi.repository import Gtk , Gdk
 from .main_box import *
 
 class MainFrame(Gtk.Window):
-    def __init__(self, common):
+	def __init__(self, common):
 
-        Gtk.Window.__init__(self, title="Votre Agenda")
-        self.add(MainBox(common))
-        self.set_name("MainFrame")
+		Gtk.Window.__init__(self, title="Votre Agenda")
+		self.add(MainBox(common))
+		self.set_name("MainFrame")
 
-        #pour la démo:
-        #self.fullscreen()
+		#pour la démo:
+		#self.fullscreen()
+		self.maximize()
 
-        #Initialisation d'un fichier CSS:
-        style_provider = Gtk.CssProvider()
-        css = open('client/view/css/style.css' ,'rb')
-        css_data = css.read()
-        css.close()
+		#Initialisation d'un fichier CSS:
+		style_provider = Gtk.CssProvider()
+		css = open('client/view/css/style.css' ,'rb')
+		css_data = css.read()
+		css.close()
 
-        style_provider.load_from_data(css_data)
+		style_provider.load_from_data(css_data)
 
-        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),style_provider,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+		Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),style_provider,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
